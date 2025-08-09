acf9e7bbeff1051c7e2a504dad57ad753c49a026
崔志阳 <1459645586@qq.com>
2025-08-09 21:18:19 +0800

    fix: 修正git提交步骤状态更新预期外问题

--- src/main/java/com/cuizhy/rhc/service/WorkService.java
+++ src/main/java/com/cuizhy/rhc/service/WorkService.java
@@ -133,7 +133,7 @@ public class WorkService {
         gitService.copyFile(info);
     }
 
-    @ProcessPoint(progress = Constants.JOB_PROGRESS_GIT_COMMIT_AND_PUSH, phase = TaskPhase.FINISH)
+    @ProcessPoint(progress = Constants.JOB_PROGRESS_GIT_COMMIT_AND_PUSH)
     public void gitCommitAndPush(TaskSubmitPVO taskSubmitPVO){
         String env = taskSubmitPVO.getEnv();
         String work = taskSubmitPVO.getWork();
