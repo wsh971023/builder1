dfaedf898dba0df229af884c9148fedb54798c3a
崔志阳 <1459645586@qq.com>
2025-09-03 23:02:09 +0800

    应对二进制提交随时间膨胀问题，每次删除原分支，重新创建孤儿分支更名后强制推送覆盖。依赖于git服务器自带的整理功能进行仓库容量清理

--- src/main/java/com/cuizhy/rhc/service/GitService.java
+++ src/main/java/com/cuizhy/rhc/service/GitService.java
@@ -64,7 +64,7 @@ public class GitService {
     @SneakyThrows
     public void gitCommitAndPush(Info info){
         this.gitCommit(info.getRepoUrl());
-        GitUtil.gitPush(info.getRepoUrl(),getUserName(),getGenerateToken());
+        GitUtil.forceCleanAndPush(info.getRepoUrl(),getUserName(),getGenerateToken());
     }
 
     @SneakyThrows
--- src/main/java/com/cuizhy/rhc/util/GitUtil.java
+++ src/main/java/com/cuizhy/rhc/util/GitUtil.java
@@ -3,8 +3,11 @@ package com.cuizhy.rhc.util;
 import com.cuizhy.rhc.constants.Constants;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PushCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.TextProgressMonitor;
+import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 
 import java.io.File;
@@ -104,5 +107,49 @@ public class GitUtil {
 
         // 使用Paths.get来跨平台地、安全地构建文件路径
         return Paths.get(FileUtil.getRuntimeAbsolutePath(), Constants.GIT_CLONE_DIR, repoName).toString();
+    }
+
+    public static void forceCleanAndPush(String remote_repo_url,String username,String password) throws IOException, GitAPIException {
+        String local_repo_dir = getCloneDir(remote_repo_url);
+        try (Git git = Git.open(new File(local_repo_dir))) {
+            Repository repository = git.getRepository();
+            //自动检测当前分支名称
+            String targetBranchName = repository.getBranch();
+
+            Iterable<RevCommit> log = git.log().setMaxCount(1).call();
+            RevCommit latestCommit = log.iterator().next();
+            //获取最近一次提交信息
+            String originalCommitMessage = latestCommit.getFullMessage();
+
+            String tempBranchName = "new-history-" + System.currentTimeMillis();
+            System.out.println("\n--- 步骤 2/5: 正在创建新的孤儿分支 '" + tempBranchName + "'... ---");
+            git.checkout().setOrphan(true).setName(tempBranchName).call();
+
+            // 步骤 3: 将 depth 1 的内容提交到新分支
+            System.out.println("--- 步骤 3/5: 正在将最新文件作为初始提交... ---");
+            git.add().addFilepattern(".").call();
+            git.commit().setMessage(originalCommitMessage).call();
+            System.out.println("新历史的初始提交已创建。");
+
+            // 步骤 4 & 5: 将新历史强制应用到目标分支，并清理临时分支
+            System.out.println("--- 步骤 4/5: 正在将新历史强制更新到 '" + targetBranchName + "'... ---");
+            // 使用 branchCreate() 和 setForce(true) 来模拟 'git branch -M'。
+            // 这会强制将 targetBranchName 分支的指针移动到我们当前所在的新历史的 HEAD
+            git.branchCreate()
+                    .setName(targetBranchName)
+                    .setForce(true)
+                    .call();
+
+            // 切换到更新后的目标分支
+            git.checkout().setName(targetBranchName).call();
+
+            // 删除不再需要的临时分支
+            git.branchDelete().setBranchNames(tempBranchName).call();
+            System.out.println("分支 '" + targetBranchName + "' 已成功指向新历史。");
+
+            // 步骤 6: 强制推送
+            System.out.println("--- 步骤 5/5: 正在强制推送到远程仓库... ---");
+            git.push().setForce(true).add(targetBranchName).setCredentialsProvider(new UsernamePasswordCredentialsProvider(username, password)).call();
+        }
     }
 }
